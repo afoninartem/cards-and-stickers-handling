bb9e6050044d9d05be58f0c48bd1dc95dbffbba3
afoninartem <47443887+afoninartem@users.noreply.github.com>
2020-07-29 17:55:29 +0300

    final

--- js.js
+++ js.js
@@ -1,20 +1,65 @@
 const list = [];
 const shops = [];
-let cardSum = 0, stickerSum = 0; //perhabs I should set it to onchange function
+let cardSum = 0, stickerSum = 0;
+
+const printTable = () => {
+  const originBody = document.body.innerHTML;
+  const tableToPrint = document.querySelector('.print-block').innerHTML;
+  document.body.innerHTML = tableToPrint;
+  window.print();
+  document.body.innerHTML = originBody;
+}
+
+const createPreview = () => {
+  const printBlock = document.querySelector('.print-block');
+  printBlock.style.display = 'grid';
+  const hiddenButton = document.querySelector('.hidden__button');
+  hiddenButton.style.display = 'flex';
+  list.sort();
+  list.forEach(elem => {
+    const arr = elem.split(';');
+    const shopName = document.createElement('div');
+    shopName.classList.add('shop-name');
+    shopName.textContent = arr[0];
+    printBlock.appendChild(shopName);
+    const shopCards = document.createElement('div');
+    shopCards.classList.add('quantity');
+    shopCards.textContent = arr[1];
+    printBlock.appendChild(shopCards);
+    const shopStickers = document.createElement('div');
+    shopStickers.classList.add('quantity');
+    shopStickers.textContent = arr[2];
+    printBlock.appendChild(shopStickers);
+  });
+  const summ = document.createElement('div');
+  summ.classList.add('title');
+  summ.textContent = `ИТОГО`;
+  printBlock.appendChild(summ);
+  const summCards = document.createElement('div');
+  summCards.classList.add('title');
+  summCards.textContent = cardSum;
+  printBlock.appendChild(summCards);
+  const summStickers = document.createElement('div');
+  summStickers.classList.add('title');
+  summStickers.textContent = stickerSum;
+  printBlock.appendChild(summStickers);
+}
 
 const checkShopName = (name) => {
-  console.log(typeof name, name)
-  const namePart = name.match(/(_)\w+/g);
-  console.log(namePart)
+  const namePart = name.match(/_.+/g);
+  shops.forEach((elem, i) => {
+    // console.log(elem)
+    if (elem.includes(namePart)) name = shops[i];
+  });
+  return name;
 }
 
 const checkData = (row) => {
-  const card = row[8]||0, sticker = row[9]||0;
+  const card = row[8] || 0, sticker = row[9] || 0;
   if (+card > 0 || +sticker > 0) {
     cardSum += +card;
     stickerSum += +sticker;
-    console.log(`${checkShopName(row[1])};${card};${sticker}`);
-    return list.push(`${row[1]};${card};${sticker}`);
+    return list.push(`${checkShopName(row[1])};${card};${sticker}`);
   }
 }
 
@@ -40,20 +85,24 @@ document.getElementById('list').onchange = function () {
       row = Array.from(row.split(';'));
       const str = checkData(row);
     });
+    createPreview();
   }
   reader.readAsText(file, 'windows-1251');
 }
 
 document.querySelector('#download').onclick = function () {
-  let csv = `Салон;Визитки;Наклейки\n`;
-  list.forEach(el => {
-    csv += el;
-    csv += `\n`;
-  });
-  csv += `ИТОГО:;${cardSum};${stickerSum}`;
-  var hiddenElement = document.createElement('a');
-  hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI("\uFEFF" + csv);
-  hiddenElement.target = '_blank';
-  hiddenElement.download = 'Отгрузка визиток и наклеек.csv';
-  hiddenElement.click();
+  if (document.querySelector('#list').value !== '') {
+    let csv = `Салон;Визитки;Наклейки\n`;
+    list.sort();
+    list.forEach(el => {
+      csv += el;
+      csv += `\n`;
+    });
+    csv += `ИТОГО:;${cardSum};${stickerSum}`;
+    var hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI("\uFEFF" + csv);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'Отгрузка визиток и наклеек.csv';
+    hiddenElement.click();
+  }
 }
